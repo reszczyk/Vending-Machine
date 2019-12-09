a3bbe754704d78dc991992ccb5d14cde618a9ab2
PR <patrykreszczynski@gmail.com>
2019-12-09 15:35:00 +0100

    Cash fixed

--- VendingMachine/Cash.cs
+++ VendingMachine/Cash.cs
@@ -26,36 +26,27 @@ namespace VendingMachine.VendingMachine
         public void ReturnMoney()
         {
             Console.WriteLine("Wydano:");
-            int zl5 = 0;
-            int zl2 = 0;
-            int zl1 = 0;
-            int gr50 = 0;
-            int gr20 = 0;
-            int gr10 = 0;
+            this.Money = ReturnZl(5, this.Money, "5");
+            this.Money = ReturnZl(2, this.Money, "2");
+            this.Money = ReturnZl(1, this.Money, "1");
+            this.Money = ReturnGr(50, this.Money, "50");
+            this.Money = ReturnGr(20, this.Money, "20");
+            this.Money = ReturnGr(10, this.Money, "10");
+        }
 
-            zl5 = Decimal.ToInt32(this.Money / 5);
-            if(zl5 != 0) { Console.WriteLine(zl5 + "x 5zł"); };
-            this.Money -= Convert.ToDecimal(5 * zl5);
-
-            zl2 = Decimal.ToInt32(this.Money / 2);
-            if (zl2 != 0) { Console.WriteLine(zl2 + "x 2zł"); };
-            this.Money -= Convert.ToDecimal(2 * zl2);
-
-            zl1 = Decimal.ToInt32(this.Money / 1);
-            if (zl1 != 0) { Console.WriteLine(zl1 + "x 1zł"); };
-            this.Money -= Convert.ToDecimal(zl1);
-
-            gr50 = Decimal.ToInt32(this.Money / 0.5m);
-            if (gr50 != 0) { Console.WriteLine(gr50 + "x 50gr"); };
-            this.Money -= Convert.ToDecimal(0.5 * gr50);
-
-            gr20 = Decimal.ToInt32(this.Money / 0.2m);
-            if (gr20 != 0) { Console.WriteLine(gr20 + "x 20gr"); };
-            this.Money -= Convert.ToDecimal(0.2 * gr20);
-
-            gr10 = Decimal.ToInt32(this.Money / 0.1m);
-            if (gr10 != 0) { Console.WriteLine(gr10 + "x 10 gr"); };
-            this.Money -= Convert.ToDecimal(0.1 * gr10);
+        public decimal ReturnZl(int amount, decimal money, string coin)
+        {
+            int penny = Decimal.ToInt32(money/amount);
+            if (penny != 0) { Console.WriteLine(penny + "x " + coin + "zł"); }
+            return money -= Convert.ToDecimal(amount * penny);
+        }
+        public decimal ReturnGr(int amount, decimal money, string coin)
+        {
+            money *= 100;
+            int penny = Decimal.ToInt32(money / amount);
+            if (penny != 0) { Console.WriteLine(penny + "x " + coin + "gr"); }
+            money -= Convert.ToDecimal(amount * penny);
+            return money /= 100;
         }
     }
 }
